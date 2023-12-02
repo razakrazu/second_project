ca2303b05748450960efe7668f1f682b0959c446
Razak <razakrazu135gmail.com>
2023-12-02 19:09:52 +0530

    first commit

---
+++ lib/login/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:secand_project/login/singup/singup_screen.dart';
+
+class Login_Screen extends StatelessWidget {
+  const Login_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('lib/assets/fkljk.jpg'), fit: BoxFit.cover),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(),
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15, top: 150),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 174, 172, 172)),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    height: 500,
+                    width: 500,
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 70,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(90),
+                              ),
+                              hintText: 'Email',
+                              hintStyle: const TextStyle(
+                                color: Colors.white,
+                              ),
+                              prefixIcon: const Icon(Icons.mail_outline,color: Color.fromARGB(255, 200, 200, 200),),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              
+                              border: OutlineInputBorder(
+                                borderSide:const BorderSide(color:  Colors.white),
+                                borderRadius: BorderRadius.circular(90),
+                              ),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(
+                                color: Colors.white,
+                              ),
+                                  prefixIcon: const Icon(Icons.lock_outlined,color: Color.fromARGB(255, 200, 200, 200),),
+
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 200),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text('forgot password',
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 80,
+                            right: 80,
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Log In',
+                              style:const TextStyle(color: Colors.white),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: TextButton(
+                            onPressed: () {
+                               Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Singup_Screen()),
+            );
+                            },
+                            child: const Text('Create New Account',
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
---
+++ lib/login/singup/singup_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Singup_Screen extends StatelessWidget {
+  const Singup_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
